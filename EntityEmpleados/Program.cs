852fb6f3ddb2eb688bc17955ae006f89b94db917
FranLsz <franlsz95develop@gmail.com>
2015-10-27 14:16:55 +0100

    first update

---
+++ EntityEmpleados/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity.Core.Metadata.Edm;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EntityEmpleados.Model;
+
+namespace EntityEmpleados
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var ctx = new EmpleadosEntities();
+            var p = new Puesto()
+            {
+                nombre = "Tecnico IT",
+                salarioBase = 30000
+
+            };
+
+            ctx.Puesto.Add(p);
+        }
+    }
+}
